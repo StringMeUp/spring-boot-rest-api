fde0dc90a16fb47c9034921ddb21e5aa9280317f
Samir Ramic <samir@weloveapps.de>
2024-07-16 12:35:38 +0200

    [SR-10 JPA Testing with @ParameterizedTest]

--- src/test/intg/com/kotlin/spring/sr_kotlin_catalog_service/repository/CourseRepositoryIntgTest.kt
+++ src/test/intg/com/kotlin/spring/sr_kotlin_catalog_service/repository/CourseRepositoryIntgTest.kt
@@ -1,11 +1,16 @@
 package com.kotlin.spring.sr_kotlin_catalog_service.repository
 
 import com.kotlin.spring.sr_kotlin_catalog_service.util.courseEntityList
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.provider.MethodSource
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
 import org.springframework.test.context.ActiveProfiles
+import java.util.stream.Stream
 import kotlin.test.assertEquals
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -32,5 +37,21 @@ class CourseRepositoryIntgTest {
     fun findByName_test() {
         val result = repository.findByName("Tes")
         assertEquals(3, result.size)
+    }
+
+    @ParameterizedTest
+    @MethodSource("courseAndSize")
+    fun findByNameTest_parametrized(name: String, expectedSize: Int) {
+        val courses = repository.findByName(name)
+        println("Courses $courses")
+
+        Assertions.assertEquals(expectedSize, courses.size)
+    }
+
+    companion object {
+        @JvmStatic
+        fun courseAndSize(): Stream<Arguments> {
+            return Stream.of(Arguments.arguments("Test", 3), Arguments.arguments("Bin", 1))
+        }
     }
 }
